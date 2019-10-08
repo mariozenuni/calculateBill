1741d103597b97db26ba15bb15dfbfc248ffbc66
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2019-10-08 23:23:43 +0100

    Add some pseudocode to test file

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+// - Calculate Bill - Create a function that takes the price of the bill, the VAT rate and the tip 
+// rate and returns the total amount the customer needs to pay, formatted as a currency string.
+
+
+// create a function
+// takes price, VAT and tip rate as numbers
+// retuns the total ammout formated as a currency string
+// ( amount + (amount * vat / 100) ) + tip )
