a85acf3e497d2d9adff885aae9ea2c2db50b99d0
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2019-10-08 23:36:31 +0100

    Add implementation of type checking for function arguments

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const calculateBill = (price, vat, tip) => {
+    if (typeof price !== 'number') {
+        return "error";
+    }
+
+    if (typeof vat !== 'number') {
+        return "error";
+    }
+
+    if (typeof tip !== 'number') {
+        return "error";
+    }
+    
     const bill = price + (price * vat / 100) + tip;
 
     return "£" + bill;
