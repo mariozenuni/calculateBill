042f9e760c2053e6e3698fe142410650a3d61a55
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2019-10-08 23:33:36 +0100

    Add implementation to fix test - still failing as expects £130 string and receives 130 number

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const calculateBill = () => 'I am a string'
+const calculateBill = (price, vat, tip) => {
+    const bill = price + (price * vat / 100) + tip;
+
+    return bill;
+}
 
 module.exports =  calculateBill;
